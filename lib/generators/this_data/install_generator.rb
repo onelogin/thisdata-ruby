fa729897ab43fcf3665cfdf4fcf10dbb0435e516
Nick Malcolm <nick@revert.io>
2016-02-22 09:21:55 +1300

    config comments

--- lib/generators/this_data/install_generator.rb
+++ lib/generators/this_data/install_generator.rb
@@ -10,11 +10,18 @@ module ThisData
 ThisData.setup do |config|
   config.api_key = "#{api_key}"
 
+  # user_method will be called on a controller when using TrackRequest
   # config.user_method =        :current_user
+
+  # The following methods will be called on the object returned by user_method,
+  # to capture details about the user
   # config.user_id_method =     :id
   # config.user_name_method =   :name
   # config.user_email_method =  :email
   # config.user_mobile_method = :mobile
+
+  # Define a Logger instance if you want to debug / track errors
+  # config.logger = Rails.logger unless Rails.env.production?
 end
 EOS
       end
