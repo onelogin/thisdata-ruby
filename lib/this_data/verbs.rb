2f950acc5e8d04699211afe4cb4526c58568b7b0
Nick Malcolm <nick@malcolm.net.nz>
2016-05-03 09:17:34 +1200

    Add access, and adjust alignment

--- lib/this_data/verbs.rb
+++ lib/this_data/verbs.rb
@@ -8,20 +8,21 @@
 module ThisData
   class Verbs
 
-
     LOG_IN                = 'log-in'
     LOG_OUT               = 'log-out'
     LOG_IN_DENIED         = 'log-in-denied'
     LOG_IN_CHALLENGE      = 'log-in-challenge'
 
-    EMAIL_UPDATE = 'email-update'
-    PASSWORD_UPDATE = 'password-update'
+    ACCESS                = 'access'
 
-    PASSWORD_RESET_REQUEST = 'password-reset-request'
-    PASSWORD_RESET = 'password-reset'
-    PASSWORD_RESET_FAIL = 'password-reset-fail'
+    EMAIL_UPDATE          = 'email-update'
+    PASSWORD_UPDATE       = 'password-update'
 
-    AUTHENTICATION_CHALLENGE = 'authentication-challenge'
+    PASSWORD_RESET_REQUEST  = 'password-reset-request'
+    PASSWORD_RESET          = 'password-reset'
+    PASSWORD_RESET_FAIL     = 'password-reset-fail'
+
+    AUTHENTICATION_CHALLENGE      = 'authentication-challenge'
     AUTHENTICATION_CHALLENGE_PASS = 'authentication-challenge-pass'
     AUTHENTICATION_CHALLENGE_FAIL = 'authentication-challenge-fail'
 
