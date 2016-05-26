3c1c57735b172ed00a5ec900c8efa3da071162c7
richet <richardchetwynd@gmail.com>
2016-05-27 10:25:24 +1200

    uri encode

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,7 @@ class ThisData::UnitTest < Minitest::Test
   end
 
   def register_successful_events
-    successful_path = "https://api.thisdata.com/v1/events?api_key=#{ThisData.configuration.api_key}"
+    successful_path = URI.encode("https://api.thisdata.com/v1/events?api_key=#{ThisData.configuration.api_key}")
     FakeWeb.register_uri(:post, successful_path, body: "", status: 202)
   end
 
